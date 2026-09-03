6848a2ca078e7b7d665a1ebdb560c222e4e6837b
David Gray <davidagray2005@gmail.com>
2026-09-02 18:55:43 -0800

    Created imports

---
+++ frontend/src/app/dataset.service.ts
@@ -0,0 +1,5 @@
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map, Observable } from 'rxjs';
+
+import { BandStatistics, DatasetMetadata } from './dataset';
